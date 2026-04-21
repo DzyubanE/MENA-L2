b84064a06211446646bfd2653adf717e77fad346
DzyubanE <dzyuban.e@mailbox366.com>
2026-04-21 14:26:26 +0300

    beta release

--- apply-confirm.user.js
+++ apply-confirm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Edit Helper Team B
+// @name         Edit Helper Team B BETA
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/DzyubanE/MENA-L2/raw/refs/heads/main/apply-confirm.user.js
 // @downloadURL  https://github.com/DzyubanE/MENA-L2/raw/refs/heads/main/apply-confirm.user.js
 // @description  Двойное подтверждение + шаблоны комментариев
